adbb9901973c2cfd9f605712e1240a6a8ba8ce87
nicollas8 <nicollas.msantos1@gmail.com>
2023-09-01 10:53:48 -0300

    Initial comitt

---
+++ js/firebase-init.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2J40UU-zVhIF5uvHgsyYyAd-qIegz5QQ",
+    authDomain: "donasdoninho.firebaseapp.com",
+    projectId: "donasdoninho",
+    storageBucket: "donasdoninho.appspot.com",
+    messagingSenderId: "775885688659",
+    appId: "1:775885688659:web:93a137be1d6443b9041ba6",
+    measurementId: "G-6H97D5FZNE"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  
---
+++ js/script.js
@@ -0,0 +1,97 @@
+function onChangeEmail () {
+    toggleBtnDisabled()
+    toggleEmailErrors ()
+}
+
+function onChangeSenha () {
+toggleBtnDisabled()
+toggleSenhaErrors()
+}
+
+function isEmailValid () {
+    const email = form.email().value
+    if (!email) {
+        return false
+    }   
+    return validateEmail(email)
+}
+
+function isPasswordValid () {
+    const password = form.senha().value
+ if (!password) {
+    return false
+} 
+return true 
+}
+
+function validateEmail (email) {
+    return /\S+@\S+\.\S+/.test(email)
+}
+
+function toggleEmailErrors () {
+    const email = form.email().value
+    form.erroEmailRequired().style.display = email ? "none" : "block"
+    form.erroEmailInvalid().style.display = email ? "none" : "block"
+}
+
+function toggleSenhaErrors () {
+    const password = form.senha().value
+    form.erroSenhaRequired().style.display = password ? "none" : "block"
+}
+
+
+function toggleBtnDisabled () {
+    const emailValid = isEmailValid()
+    form.btnRecuperarSenha().disabled = !emailValid
+    
+    const passwordValid = isPasswordValid()
+    form.btnLogin().disabled = !emailValid  || !isPasswordValid
+}
+
+    const form = {
+        email: () => document.getElementById('email'),
+        erroEmailInvalid: () => document.getElementById ('erroEmailInvalid'),
+        erroEmailRequired: () => document.getElementById ('erroEmailRequired'),
+        senha: () => document.getElementById('senha'),
+        erroSenhaRequired: () => document.getElementById('erroSenhaRequired'),
+        btnRecuperarSenha: () => document.getElementById('btnRecuperarSenha'),
+        btnLogin: () => document.getElementById("btnLogin")
+
+    }
+
+function login () {
+    firebase.auth().signInWithEmailAndPassword (
+        form.email().value, form.senha().value
+        ).then (response => {
+            window.location.href = "telaInicio.html";
+        }).catch(error => {
+            alert(getErrorMessage(error));
+            
+        });
+    }
+
+function getErrorMessage (error){
+    if (error.code == "auth/user-not-found") {
+        return "Usuário não encontrado";
+    } else if (error.code == "auth/wrong-password") {
+        return "Senha Incorreta";
+    }
+    return error.message;
+}
+
+function recoverPassword () {
+    firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
+        alert ("E-mail enviado com sucesso");
+        window.location.href = "telaLogin.html"
+    }).catch (error => {
+        alert(getErrorMessage (error));
+    })
+}
+
+function registrar () {
+    window.location.href = "cadastroInfo.html"
+}
+
+function recoverySenha () {
+    window.location.href = "telaRecuperarSenha.html"
+}
