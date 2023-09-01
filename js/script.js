70d7051a436845443911974cc8ca451acee7f1cb
nicollas8 <nicollas.msantos1@gmail.com>
2023-09-01 20:22:31 -0300

    alteração cadastro

--- js/script.js
+++ js/script.js
@@ -1,76 +1,76 @@
-function onChangeEmail () {
+function onChangeEmail() {
     toggleBtnDisabled()
-    toggleEmailErrors ()
+    toggleEmailErrors()
 }
 
-function onChangeSenha () {
-toggleBtnDisabled()
-toggleSenhaErrors()
+function onChangeSenha() {
+    toggleBtnDisabled()
+    toggleSenhaErrors()
 }
 
-function isEmailValid () {
+function isEmailValid() {
     const email = form.email().value
     if (!email) {
         return false
-    }   
+    }
     return validateEmail(email)
 }
 
-function isPasswordValid () {
+function isPasswordValid() {
     const password = form.senha().value
- if (!password) {
-    return false
-} 
-return true 
+    if (!password) {
+        return false
+    }
+    return true
 }
 
-function validateEmail (email) {
+function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email)
 }
 
-function toggleEmailErrors () {
+function toggleEmailErrors() {
     const email = form.email().value
     form.erroEmailRequired().style.display = email ? "none" : "block"
     form.erroEmailInvalid().style.display = email ? "none" : "block"
 }
 
-function toggleSenhaErrors () {
+function toggleSenhaErrors() {
     const password = form.senha().value
     form.erroSenhaRequired().style.display = password ? "none" : "block"
 }
 
 
-function toggleBtnDisabled () {
+function toggleBtnDisabled() {
     const emailValid = isEmailValid()
     form.btnRecuperarSenha().disabled = !emailValid
-    
+
     const passwordValid = isPasswordValid()
-    form.btnLogin().disabled = !emailValid  || !isPasswordValid
+    form.btnLogin().disabled = !emailValid || !isPasswordValid
 }
 
-    const form = {
-        email: () => document.getElementById('email'),
-        erroEmailInvalid: () => document.getElementById ('erroEmailInvalid'),
-        erroEmailRequired: () => document.getElementById ('erroEmailRequired'),
-        senha: () => document.getElementById('senha'),
-        erroSenhaRequired: () => document.getElementById('erroSenhaRequired'),
-        btnRecuperarSenha: () => document.getElementById('btnRecuperarSenha'),
-        btnLogin: () => document.getElementById("btnLogin")
+const form = {
+    email: () => document.getElementById('email'),
+    erroEmailInvalid: () => document.getElementById('erroEmailInvalid'),
+    erroEmailRequired: () => document.getElementById('erroEmailRequired'),
+    senha: () => document.getElementById('senha'),
+    erroSenhaRequired: () => document.getElementById('erroSenhaRequired'),
+    btnRecuperarSenha: () => document.getElementById('btnRecuperarSenha'),
+    btnLogin: () => document.getElementById("btnLogin")
 
-    }
+}
 
-function login () {
-    firebase.auth().signInWithEmailAndPassword (
+function login() {
+    firebase.auth().signInWithEmailAndPassword(
         form.email().value, form.senha().value
-        ).then (response => {
-            window.location.href = "telaInicio.html";
-        }).catch(error => {
-            alert(getErrorMessage(error));
-            
-        });
-    }
+    ).then(response => {
+        window.location.href = "telaInicio.html";
+    }).catch(error => {
+        alert(getErrorMessage(error));
 
-function getErrorMessage (error){
+    });
+}
+
+function getErrorMessage(error) {
     if (error.code == "auth/user-not-found") {
         return "Usuário não encontrado";
     } else if (error.code == "auth/wrong-password") {
@@ -79,19 +79,68 @@ function getErrorMessage (error){
     return error.message;
 }
 
-function recoverPassword () {
+function recoverPassword() {
     firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
-        alert ("E-mail enviado com sucesso");
+        alert("E-mail enviado com sucesso");
         window.location.href = "telaLogin.html"
-    }).catch (error => {
-        alert(getErrorMessage (error));
+    }).catch(error => {
+        alert(getErrorMessage(error));
     })
 }
 
-function registrar () {
+function registrar() {
     window.location.href = "cadastroInfo.html"
 }
 
-function recoverySenha () {
+function recoverySenha() {
     window.location.href = "telaRecuperarSenha.html"
+}
+
+// validação cadastro 
+
+const formEMailCadastro = document.getElementById('formEmail');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+const emailRegex = /\S+@\S+\.\S+/;
+
+formEMailCadastro.addEventListener('submit', (event) => {
+    event.defaultPrevented();
+    emailValidateCadastro();
+    mainPasswordValidate();
+    comparePassword();
+});
+
+function setError(index) {
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
+}
+
+function removeError(index) {
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function emailValidateCadastro() {
+    if (!emailRegex.test(campos[0].value)) {
+        setError(0);
+    } else {
+        removeError(0);
+    }
+}
+
+function mainPasswordValidate() {
+    if (campos[1].value.length < 8) {
+        setError(1);
+    } else {
+        removeError(1);
+        comparePassword();
+    }
+}
+
+function comparePassword() {
+    if (campos[1].value == campos[2].value && campos[2].value.length >= 8) {
+        removeError(2);
+    } else {
+        setError(2)
+    }
 }
